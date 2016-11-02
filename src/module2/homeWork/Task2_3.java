e327820005470d8a706e610590ff52339176c3ab
andrey.demchuk <andreichdem@gmail.com>
2016-11-02 14:27:46 +0200

    Task 2_3 complete

---
+++ src/module2/homeWork/Task2_3.java
@@ -0,0 +1,40 @@
+package module2.homeWork;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Task2_3 {
+    public Task2_3() {
+    }
+
+    public static void main(String[] args) {
+        int[] balances = new int[]{1200, 250, 2000, 500, 3200};
+        String[] ownerNames = new String[]{"Jane", "Ann", "Jack", "Oww", "Lane"};
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter name " + Arrays.toString(ownerNames) + "\n");
+        String name = sc.nextLine();
+        System.out.println("Enter withdrawal");
+        double withdrawal = sc.nextDouble();
+        withdrawMoney(ownerNames, balances, name, withdrawal);
+    }
+
+    static void withdrawMoney(String[] owner, int[] balanceOwner, String name, double withdrawal) {
+        double commision = 5.0D * withdrawal / 100.0D;
+        int counter = 0;
+
+        for(int total = 0; total < owner.length; ++total) {
+            if(name.equals(owner[total])) {
+                counter = total;
+                break;
+            }
+        }
+
+        double var10 = (double)balanceOwner[counter] - withdrawal - commision;
+        if(var10 >= 0.0D) {
+            System.out.println(name + " " + withdrawal + " " + var10);
+        } else {
+            System.out.println(name + " NO");
+        }
+
+    }
+}
