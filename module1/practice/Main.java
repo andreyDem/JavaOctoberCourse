8db98e4fe6dbd129226484788adc596d549c2c10
andrey.demchuk <andreichdem@gmail.com>
2016-11-02 10:57:39 +0200

    add src folder

--- module1/practice/Main.java
+++
@@ -1,9 +0,0 @@
-package module1.practice;
-
-public class Main {
-    public static void main(String[] args) {
-        //System.out.println("test");
-        /*System.out.println();*/
-        System.out.println("ppp");
-    }
-}
