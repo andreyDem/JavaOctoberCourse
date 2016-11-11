67519887850a8822a46732fd15ec091c10bb283d
andrey.demchuk <andreichdem@gmail.com>
2016-11-11 12:00:38 +0200

    module3 task3.2 complete + Main test

---
+++ src/module3/homeWork/task3_2/Main.java
@@ -0,0 +1,12 @@
+package module3.homeWork.task3_2;
+public class Main {
+    public static void main(String[] args) {
+        Arithmetic arithmetic = new Arithmetic();
+
+        System.out.println(arithmetic.add(5, 10));
+
+        Adder adder = new Adder();
+
+        System.out.println(adder.check(5, 10));
+    }
+}
