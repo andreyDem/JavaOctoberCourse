ab970bf513b78f453d8ca73e458305977a691e4f
andrey.demchuk <andreichdem@gmail.com>
2016-11-11 13:42:28 +0200

    task3.3 complete

---
+++ src/module3/homeWork/task3_3/SpecialStudent.java
@@ -0,0 +1,35 @@
+package module3.homeWork.task3_3;
+public class SpecialStudent extends CollegeStudent{
+    private long secretKey;
+    private String email;
+
+
+    public SpecialStudent(String firstName, String lastName, int group) {
+        super(firstName, lastName, group);
+    }
+
+    public SpecialStudent(String lastName, Course[] coursesTaken) {
+        super(lastName, coursesTaken);
+    }
+
+    public SpecialStudent(String firstName, String lastName, int group, String collegeName, int rating, long id, long secretKey) {
+        super(firstName, lastName, group, collegeName, rating, id);
+        this.secretKey = secretKey;
+    }
+
+    public long getSecretKey() {
+        return secretKey;
+    }
+
+    public void setSecretKey(long secretKey) {
+        this.secretKey = secretKey;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
