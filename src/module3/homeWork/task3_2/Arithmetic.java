3328ffa9341f5e3c6a506043642c4a335d2d01bc
andrey.demchuk <andreichdem@gmail.com>
2016-11-12 22:14:55 +0200

    task3.2 fix corrections

--- src/module3/homeWork/task3_2/Arithmetic.java
+++ src/module3/homeWork/task3_2/Arithmetic.java
@@ -1,9 +1,6 @@
 package module3.homeWork.task3_2;
 public class Arithmetic {
 
-    private Integer a;
-    private Integer b;
-
 
     int add(Integer a, Integer b){
         return a + b;
--- src/module3/homeWork/task3_2/Main.java
+++ src/module3/homeWork/task3_2/Main.java
@@ -1,11 +1,9 @@
 package module3.homeWork.task3_2;
 public class Main {
     public static void main(String[] args) {
-        Arithmetic arithmetic = new Arithmetic();
-
-        System.out.println(arithmetic.add(5, 10));
-
         Adder adder = new Adder();
+
+        System.out.println(adder.add(5, 10));
 
         System.out.println(adder.check(5, 10));
     }
