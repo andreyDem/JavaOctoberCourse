1d50b32e0bc93046f09f336e8df920d9e7b2be97
andrey.demchuk <andreichdem@gmail.com>
2016-11-13 22:33:10 +0200

    task3.2 fix corrections

--- src/module3/homeWork/task3_2/Adder.java
+++ src/module3/homeWork/task3_2/Adder.java
@@ -1,10 +1,6 @@
 package module3.homeWork.task3_2;
 
 public class Adder extends Arithmetic {
-    @Override
-    int add(Integer a, Integer b) {
-        return super.add(a, b);
-    }
 
     boolean check(Integer a, Integer b) {
         if (a >= b)
--- src/module3/homeWork/task3_2/Arithmetic.java
+++ src/module3/homeWork/task3_2/Arithmetic.java
@@ -1,7 +1,6 @@
 package module3.homeWork.task3_2;
 public class Arithmetic {
 
-
     int add(Integer a, Integer b){
         return a + b;
     }
