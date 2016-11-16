b637ee080f027307dac01f1d1d0fd2303327e8c7
andrey.demchuk <andreichdem@gmail.com>
2016-11-16 13:17:05 +0200

    practice 3 Task 4.1 started

--- src/module3/practice/task4_1/WorkerFixedPayment.java
+++ src/module3/practice/task4_1/WorkerFixedPayment.java
@@ -2,6 +2,7 @@ package module3.practice.task4_1;
 
 public class WorkerFixedPayment extends Worker{
 
+
     @Override
     double averageMonthlySalary() {
         return getSalary();
--- src/module3/practice/task4_1/WorkerHourlyPayment.java
+++ src/module3/practice/task4_1/WorkerHourlyPayment.java
@@ -3,6 +3,10 @@ package module3.practice.task4_1;
 public class WorkerHourlyPayment extends Worker{
     private double hourlyPayment;
 
+    public WorkerHourlyPayment(double hourlyPayment) {
+        this.hourlyPayment = hourlyPayment;
+    }
+
     double averageMonthlySalary() {
         return 20.8 * 8 * getHourlyPayment();
     }
