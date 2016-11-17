9487b1aaaefc61ee5735051fab3b5c599152bbf7
andrey.demchuk <andreichdem@gmail.com>
2016-11-17 14:42:33 +0200

    homeWork 4_4 complete

--- src/module4/homeWork/task4_4/User.java
+++ src/module4/homeWork/task4_4/User.java
@@ -4,14 +4,34 @@ import module4.homeWork.task4_1.Bank;
 
 public class User {
 
-    long id;
-    String name;
-    double balance;
-    int monthsOfEmployment;
-    String companyName;
-    int salary;
-    Bank bank;
+    private long id;
+    private String name;
+    private double balance;
+    private int monthsOfEmployment;
+    private String companyName;
+    private int salary;
+    private Bank bank;
 
+    public User(long id, String name, double balance, int monthsOfEmployment, String companyName, int salary, Bank bank) {
+        this.id = id;
+        this.name = name;
+        this.balance = balance;
+        this.monthsOfEmployment = monthsOfEmployment;
+        this.companyName = companyName;
+        this.salary = salary;
+        this.bank = bank;
+    }
 
-
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", balance=" + balance +
+                ", monthsOfEmployment=" + monthsOfEmployment +
+                ", companyName='" + companyName + '\'' +
+                ", salary=" + salary +
+                ", bank=" + bank +
+                '}';
+    }
 }
