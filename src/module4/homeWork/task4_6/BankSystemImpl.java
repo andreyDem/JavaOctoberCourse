e1b58419bcad18f17ef7f3817b027da3ceab2976
andrey.demchuk <andreichdem@gmail.com>
2016-11-17 16:15:50 +0200

    homeWork 4 BankSystemImpl complete

--- src/module4/homeWork/task4_6/BankSystemImpl.java
+++ src/module4/homeWork/task4_6/BankSystemImpl.java
@@ -3,25 +3,33 @@ package module4.homeWork.task4_6;
 import module4.homeWork.task4_4.User;
 
 
-public class BankSystemImpl implements BankSystem{
+public class BankSystemImpl implements BankSystem {
 
     @Override
     public void withdrawOfUser(User user, int amount) {
-        if (user.getBalance() - user.getBank().getCommission(amount) > 0);
+        double comission = (amount * user.getBank().getCommission(amount) / 100);
+        if (user.getBalance() - amount - comission >= 0)
+            user.setBalance(user.getBalance() - amount - comission);
+        else
+            System.out.println("Not enough money on your balance: " + user.getBalance());
     }
 
     @Override
     public void fundUser(User user, int amount) {
-
+        user.setBalance(user.getBalance() + amount);
     }
 
     @Override
     public void transferMoney(User fromUser, User toUser, int amount) {
-
+        if (fromUser.getBalance() >= amount) {
+            fromUser.setBalance(fromUser.getBalance() - amount);
+            toUser.setBalance(toUser.getBalance() + amount);
+        } else
+            System.out.println("Your amount: " + amount + " is bigger than your balance: " + fromUser.getBalance());
     }
 
     @Override
     public void paySalary(User user) {
-
+        user.setBalance(user.getBalance() + user.getSalary());
     }
 }
