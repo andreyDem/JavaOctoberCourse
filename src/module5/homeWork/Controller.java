bf949736b340d085e568e56cd593402c8396aa0a
andrey.demchuk <andreichdem@gmail.com>
2016-11-23 13:49:51 +0200

    module5  fix

---
+++ src/module5/homeWork/Controller.java
@@ -0,0 +1,15 @@
+package module5.homeWork;
+
+public class Controller {
+
+    API apis[] = new API[3];
+
+    Room[] requstRooms(int price, int persons, String city, String hotel){
+        return null;
+    }
+
+    Room[] check(API api1, API api2){
+        return null;
+    }
+
+}
