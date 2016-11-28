2c501834291ecc096cb40ad1fb101617a3957510
andrey.demchuk <andreichdem@gmail.com>
2016-11-28 21:11:29 +0200

    practice fix

--- src/module5/practice/Connection.java
+++ src/module5/practice/Connection.java
@@ -2,10 +2,12 @@ package module5.practice;
 
 public class Connection {
     private String url = "some url";
-    private static Connection instance = new Connection();
+    private static Connection instance;
 
     public static Connection getInstance() {
-
+        if (instance == null){
+            instance = new Connection();
+        }
         return instance;
     }
 
