e6b9979037ac7b5402a6a0d1163dbaa9834cc494
andrey.demchuk <andreichdem@gmail.com>
2016-11-30 17:27:03 +0200

    module6 homeWork complete

--- src/module2/homeWork/Task2_1.java
+++ src/module2/homeWork/Task2_1.java
@@ -1,8 +1,10 @@
 package module2.homeWork;
 
+import module6.homeWork.ArraysUtils;
+
 import java.util.Arrays;
 
-public class Task2_1 {
+public class Task2_1{
     public static void main(String[] args) {
         int[] arr1 = new int[10];
         double[] arr2 = new double[10];
@@ -17,177 +19,180 @@ public class Task2_1 {
         }
         System.out.println(Arrays.toString(arr2));
         System.out.println();
-        sum(arr1);
-        sum(arr2);
+        ArraysUtils.sum(arr1);
+        ArraysUtils.sum(arr2);
         System.out.println();
-        min(arr1);
-        min(arr2);
+        ArraysUtils.min(arr1);
+        ArraysUtils.min(arr2);
         System.out.println();
-        max(arr1);
-        max(arr2);
+        ArraysUtils.max(arr1);
+        ArraysUtils.max(arr2);
         System.out.println();
-        maxPositive(arr1);
-        maxPositive(arr2);
+        ArraysUtils.maxPositive(arr1);
+        ArraysUtils.maxPositive(arr2);
         System.out.println();
-        multiplication(arr1);
-        multiplication(arr2);
+        ArraysUtils.multiplication(arr1);
+        ArraysUtils.multiplication(arr2);
         System.out.println();
-        modulus(arr1);
-        modulus(arr2);
+        ArraysUtils. modulus(arr1);
+        ArraysUtils.modulus(arr2);
         System.out.println();
-       // secondLargest(arr1);
-        //secondLargest(arr2);
+        ArraysUtils.secondLargest(arr1);
+        ArraysUtils.secondLargest(arr2);
+        System.out.println();
+        ArraysUtils.reverse(arr1);
+        ArraysUtils.findEvenElements(arr1);
 
 
     }
 
-    static void sum(int[] arr) {
-        int sum = 0;
-        for (int i = 0; i < arr.length; i++) {
-            sum = sum + arr[i];
-        }
-        System.out.println(sum);
-    }
-
-    static void sum(double[] arr) {
-        double sum = 0;
-        for (int i = 0; i < arr.length; i++) {
-            sum = sum + arr[i];
-        }
-        System.out.println(sum);
-    }
-
-    static void min(int[] arr) {
-        int min = arr[0];
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] < min)
-                min = arr[i];
-        }
-        System.out.println(min);
-    }
-
-    static void min(double[] arr) {
-        double min = arr[0];
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] < min)
-                min = arr[i];
-        }
-        System.out.println(min);
-    }
-
-    static void max(int[] arr) {
-        int max = arr[0];
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] > max)
-                max = arr[i];
-        }
-        System.out.println(max);
-    }
-
-    static void max(double[] arr) {
-        double max = arr[0];
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] > max)
-                max = arr[i];
-        }
-        System.out.println(max);
-    }
-
-    static void maxPositive(int[] arr) {
-        int positive = -1;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > 0) {
-                positive = arr[i];
-                break;
-            }
-        }
-        if (positive < 0) {
-            System.out.println("There is no positive elements in array");
-            System.exit(0);
-        }
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > 0 && arr[i] > positive)
-                positive = arr[i];
-        }
-        System.out.println(positive);
-    }
-
-    static void maxPositive(double[] arr) {
-        double positive = -1;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > 0) {
-                positive = arr[i];
-                break;
-            }
-        }
-        if (positive < 0) {
-            System.out.println("There is no positive elements in array");
-            System.exit(0);
-        }
-
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > 0 && arr[i] > positive)
-                positive = arr[i];
-        }
-        System.out.println(positive);
-    }
-
-
-    static void multiplication(int[] arr) {
-        long mult = arr[0];
-        for (int i = 1; i < arr.length; i++) {
-            mult = mult * arr[i];
-        }
-        System.out.println(mult);
-    }
-
-    static void multiplication(double[] arr) {
-        double mult = arr[0];
-        for (int i = 1; i < arr.length; i++) {
-            mult = mult * arr[i];
-        }
-        System.out.println(mult);
-    }
-
-    static void modulus(int[] arr) {
-        if (arr[0] < 0 || arr[0] < arr[arr.length - 1] || arr[arr.length - 1] < 0)
-            System.out.println(0);
-        else
-            System.out.println(arr[0] % arr[arr.length - 1]);
-    }
-
-    static void modulus(double[] arr) {
-        if (arr[0] < 0 || arr[0] < arr[arr.length - 1] || arr[arr.length - 1] < 0)
-            System.out.println(0);
-        else
-            System.out.println(arr[0] % arr[arr.length - 1]);
-    }
-
-    static void secondLargest(int[] arr) {
-        for (int i = arr.length - 1; i > 0; i--) {
-            for (int j = 0; j < i; j++) {
-
-                if (arr[j] > arr[j + 1]) {
-                    int tmp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = tmp;
-                }
-            }
-        }
-        System.out.println(arr[arr.length - 2]);
-    }
-
-    static void secondLargest(double[] arr) {
-        for (int i = arr.length - 1; i > 0; i--) {
-            for (int j = 0; j < i; j++) {
-
-                if (arr[j] > arr[j + 1]) {
-                    double tmp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = tmp;
-                }
-            }
-        }
-        System.out.println(arr[arr.length - 2]);
-    }
+//    static void sum(int[] arr) {
+//        int sum = 0;
+//        for (int i = 0; i < arr.length; i++) {
+//            sum = sum + arr[i];
+//        }
+//        System.out.println(sum);
+//    }
+//
+//    static void sum(double[] arr) {
+//        double sum = 0;
+//        for (int i = 0; i < arr.length; i++) {
+//            sum = sum + arr[i];
+//        }
+//        System.out.println(sum);
+//    }
+//
+//    static void min(int[] arr) {
+//        int min = arr[0];
+//        for (int i = 1; i < arr.length; i++) {
+//            if (arr[i] < min)
+//                min = arr[i];
+//        }
+//        System.out.println(min);
+//    }
+//
+//    static void min(double[] arr) {
+//        double min = arr[0];
+//        for (int i = 1; i < arr.length; i++) {
+//            if (arr[i] < min)
+//                min = arr[i];
+//        }
+//        System.out.println(min);
+//    }
+//
+//    static void max(int[] arr) {
+//        int max = arr[0];
+//        for (int i = 1; i < arr.length; i++) {
+//            if (arr[i] > max)
+//                max = arr[i];
+//        }
+//        System.out.println(max);
+//    }
+//
+//    static void max(double[] arr) {
+//        double max = arr[0];
+//        for (int i = 1; i < arr.length; i++) {
+//            if (arr[i] > max)
+//                max = arr[i];
+//        }
+//        System.out.println(max);
+//    }
+//
+//    static void maxPositive(int[] arr) {
+//        int positive = -1;
+//        for (int i = 0; i < arr.length; i++) {
+//            if (arr[i] > 0) {
+//                positive = arr[i];
+//                break;
+//            }
+//        }
+//        if (positive < 0) {
+//            System.out.println("There is no positive elements in array");
+//            System.exit(0);
+//        }
+//        for (int i = 0; i < arr.length; i++) {
+//            if (arr[i] > 0 && arr[i] > positive)
+//                positive = arr[i];
+//        }
+//        System.out.println(positive);
+//    }
+//
+//    static void maxPositive(double[] arr) {
+//        double positive = -1;
+//        for (int i = 0; i < arr.length; i++) {
+//            if (arr[i] > 0) {
+//                positive = arr[i];
+//                break;
+//            }
+//        }
+//        if (positive < 0) {
+//            System.out.println("There is no positive elements in array");
+//            System.exit(0);
+//        }
+//
+//        for (int i = 0; i < arr.length; i++) {
+//            if (arr[i] > 0 && arr[i] > positive)
+//                positive = arr[i];
+//        }
+//        System.out.println(positive);
+//    }
+//
+//
+//    static void multiplication(int[] arr) {
+//        long mult = arr[0];
+//        for (int i = 1; i < arr.length; i++) {
+//            mult = mult * arr[i];
+//        }
+//        System.out.println(mult);
+//    }
+//
+//    static void multiplication(double[] arr) {
+//        double mult = arr[0];
+//        for (int i = 1; i < arr.length; i++) {
+//            mult = mult * arr[i];
+//        }
+//        System.out.println(mult);
+//    }
+//
+//    static void modulus(int[] arr) {
+//        if (arr[0] < 0 || arr[0] < arr[arr.length - 1] || arr[arr.length - 1] < 0)
+//            System.out.println(0);
+//        else
+//            System.out.println(arr[0] % arr[arr.length - 1]);
+//    }
+//
+//    static void modulus(double[] arr) {
+//        if (arr[0] < 0 || arr[0] < arr[arr.length - 1] || arr[arr.length - 1] < 0)
+//            System.out.println(0);
+//        else
+//            System.out.println(arr[0] % arr[arr.length - 1]);
+//    }
+//
+//    static void secondLargest(int[] arr) {
+//        for (int i = arr.length - 1; i > 0; i--) {
+//            for (int j = 0; j < i; j++) {
+//
+//                if (arr[j] > arr[j + 1]) {
+//                    int tmp = arr[j];
+//                    arr[j] = arr[j + 1];
+//                    arr[j + 1] = tmp;
+//                }
+//            }
+//        }
+//        System.out.println(arr[arr.length - 2]);
+//    }
+//
+//    static void secondLargest(double[] arr) {
+//        for (int i = arr.length - 1; i > 0; i--) {
+//            for (int j = 0; j < i; j++) {
+//
+//                if (arr[j] > arr[j + 1]) {
+//                    double tmp = arr[j];
+//                    arr[j] = arr[j + 1];
+//                    arr[j + 1] = tmp;
+//                }
+//            }
+//        }
+//        System.out.println(arr[arr.length - 2]);
+//    }
 }
---
+++ src/module6/homeWork/Main.java
@@ -0,0 +1,22 @@
+package module6.homeWork;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        User andrey = new User(1, "Andrey", "Demchuk", 5000, 20000);
+        User andrey2 = new User(1, "Andrey", "Demchuk", 5000, 20000);
+        User aleksey = new User(2, "Aleksey", "Girya", 1000, 5000);
+        User matvey = new User(3, "Matvey", "Pe4kin", 3000, 15000);
+        User olga = new User(4, "Olga", "Danilovna", 500, 5000);
+        User empty = null;
+
+        User[] users = {andrey, andrey2, aleksey, matvey, olga, empty};
+
+        System.out.println(Arrays.toString(UserUtils.uniqueUsers(users)));
+        System.out.println(Arrays.toString(UserUtils.usersWithConditionalBalance(users, 5000)));
+        System.out.println(Arrays.toString(UserUtils.paySalaryToUsers(users)));
+        System.out.println(Arrays.toString(UserUtils.getUsersId(users)));
+        System.out.println(Arrays.toString(UserUtils.deleteEmptyUsers(users)));
+    }
+}
