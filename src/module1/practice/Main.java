5d65c3eaf234f9410ef5a1a21915315ef10a5278
andrey.demchuk <andreichdem@gmail.com>
2016-11-30 14:30:29 +0200

    fix

--- src/module1/practice/Main.java
+++ src/module1/practice/Main.java
@@ -2,8 +2,7 @@ package module1.practice;
 
 public class Main {
     public static void main(String[] args) {
-        //System.out.println("test");
+        //System.out.println("Test");
         /*System.out.println();*/
-        System.out.println("ppp");
     }
 }
