df053457b07c6229924a0448ceca5e1f1be96d1a
andrey.demchuk <andreichdem@gmail.com>
2016-12-06 10:57:12 +0200

    fix

--- src/module6/practice/Main.java
+++ src/module6/practice/Main.java
@@ -6,24 +6,16 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         int arr[] = new int[20];
-        Random random = new Random();
-
-        for (int i = 0; i < 20; i++) {
-//            arr[i] = createRandmInt(1, 5);
-            arr[i] = random.nextInt(20);
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = createRandmInt(2, 10);
         }
         System.out.println(Arrays.toString(arr));
-//        System.out.println(random.nextInt(20));
     }
 
 
     static int createRandmInt(int a, int b) {
         Random rand = new Random();
         int number = 0;
-        while (number < a && number > b){
-
-            number = rand.nextInt();
-        }
-        return number;
+        return number = rand.nextInt(Math.abs(b-a) +1);
     }
 }
