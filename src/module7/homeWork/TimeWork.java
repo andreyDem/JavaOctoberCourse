e9d8b3df603fca3a62ce66157fa2f31c06f981ba
andrey.demchuk <andreichdem@gmail.com>
2016-12-12 00:08:47 +0200

    module7 fix

--- src/module7/homeWork/TimeWork.java
+++ src/module7/homeWork/TimeWork.java
@@ -17,113 +17,112 @@ public class TimeWork {
             arrayListInt.add(i);
         }
         long timeAfter = System.currentTimeMillis();
-        System.out.println(timeAfter - timeBefore);
+        System.out.println("Time to add 10000 int elements to ArrayList: " + (timeAfter - timeBefore));
 
         timeBefore = System.currentTimeMillis();
         for (int i = 0; i < 10000; i++) {
             linkedListInt.add(i);
         }
         timeAfter = System.currentTimeMillis();
-        System.out.println(timeAfter - timeBefore);
-        ///---------------------------------------
+        System.out.println("Time to add 10000 int elements to LinkedList: " + (timeAfter - timeBefore));
+        System.out.println("-------------------------------------------------");
         timeBefore = System.currentTimeMillis();
         for (int i = 0; i < 10000; i++) {
             arrayListStr.add(i + "");
         }
         timeAfter = System.currentTimeMillis();
-        System.out.println(timeAfter - timeBefore);
+        System.out.println("Time to add 10000 string elements to ArrayList: " + (timeAfter - timeBefore));
 
         timeBefore = System.currentTimeMillis();
         for (int i = 0; i < 10000; i++) {
             linkedListStr.add(i + "");
         }
         timeAfter = System.currentTimeMillis();
-        System.out.println(timeAfter - timeBefore);
-        ///--------------------------------------
+        System.out.println("Time to add 10000 string elements to LinkedList: " + (timeAfter - timeBefore));
+        System.out.println("-------------------------------------------------");
         timeBefore = System.currentTimeMillis();
         for (int i = 0; i < 10000; i++) {
             arrayListInt.get(i);
         }
         timeAfter = System.currentTimeMillis();
-        System.out.println(timeAfter - timeBefore);
+        System.out.println("Time to get 10000 int elements to ArrayList: " + (timeAfter - timeBefore));
 
         timeBefore = System.currentTimeMillis();
         for (int i = 0; i < 10000; i++) {
             linkedListInt.get(i);
         }
         timeAfter = System.currentTimeMillis();
-        System.out.println(timeAfter - timeBefore);
-        ///--------------------------------------
+        System.out.println("Time to get 10000 int elements to LinkedList: " + (timeAfter - timeBefore));
+        System.out.println("-------------------------------------------------");
         timeBefore = System.currentTimeMillis();
         for (int i = 0; i < 10000; i++) {
             arrayListStr.get(i);
         }
         timeAfter = System.currentTimeMillis();
-        System.out.println(timeAfter - timeBefore);
+        System.out.println("Time to get 10000 string elements to ArrayList: " + (timeAfter - timeBefore));
 
         timeBefore = System.currentTimeMillis();
         for (int i = 0; i < 10000; i++) {
             linkedListStr.get(i);
         }
         timeAfter = System.currentTimeMillis();
-        System.out.println(timeAfter - timeBefore);
-        ///--------------------------------------
+        System.out.println("Time to get 10000 string elements to LinkedList: " + (timeAfter - timeBefore));
+        System.out.println("-------------------------------------------------");
         timeBefore = System.currentTimeMillis();
         for (int i = 0; i < 10000; i++) {
             arrayListInt.set(i, 1);
         }
         timeAfter = System.currentTimeMillis();
-        System.out.println(timeAfter - timeBefore);
+        System.out.println("Time to set 10000 int elements to ArrayList: " + (timeAfter - timeBefore));
 
         timeBefore = System.currentTimeMillis();
         for (int i = 0; i < 10000; i++) {
             linkedListInt.set(i, 1);
         }
         timeAfter = System.currentTimeMillis();
-        System.out.println(timeAfter - timeBefore);
-        ///--------------------------------------
+        System.out.println("Time to set 10000 int elements to LinkedList: " + (timeAfter - timeBefore));
+        System.out.println("-------------------------------------------------");
         timeBefore = System.currentTimeMillis();
         for (int i = 0; i < 10000; i++) {
             arrayListStr.set(i, "");
         }
         timeAfter = System.currentTimeMillis();
-        System.out.println(timeAfter - timeBefore);
+        System.out.println("Time to set 10000 string elements to ArrayList: " + (timeAfter - timeBefore));
 
         timeBefore = System.currentTimeMillis();
         for (int i = 0; i < 10000; i++) {
             linkedListStr.set(i, "");
         }
         timeAfter = System.currentTimeMillis();
-        System.out.println(timeAfter - timeBefore);
-        ///--------------------------------------
+        System.out.println("Time to set 10000 string elements to LinkedList: " + (timeAfter - timeBefore));
+        System.out.println("-------------------------------------------------");
         timeBefore = System.currentTimeMillis();
         for (int i = 0; i < 10000; i++) {
             arrayListInt.remove(0);
         }
         timeAfter = System.currentTimeMillis();
-        System.out.println(timeAfter - timeBefore);
+        System.out.println("Time to remove 10000 int elements to ArrayList: " + (timeAfter - timeBefore));
 
         timeBefore = System.currentTimeMillis();
         for (int i = 0; i < 10000; i++) {
             linkedListInt.remove(0);
         }
         timeAfter = System.currentTimeMillis();
-        System.out.println(timeAfter - timeBefore);
-        ///--------------------------------------
+        System.out.println("Time to remove 10000 int elements to LinkedList: " + (timeAfter - timeBefore));
+        System.out.println("-------------------------------------------------");
         timeBefore = System.currentTimeMillis();
         for (int i = 0; i < 10000; i++) {
             arrayListStr.remove(0);
         }
         timeAfter = System.currentTimeMillis();
-        System.out.println(timeAfter - timeBefore);
+        System.out.println("Time to remove 10000 string elements to ArrayList: " + (timeAfter - timeBefore));
 
         timeBefore = System.currentTimeMillis();
         for (int i = 0; i < 10000; i++) {
             linkedListStr.remove(0);
         }
         timeAfter = System.currentTimeMillis();
-        System.out.println(timeAfter - timeBefore);
-        ///--------------------------------------
+        System.out.println("Time to remove 10000 string elements to LinkedList: " + (timeAfter - timeBefore));
 
     }
 }
