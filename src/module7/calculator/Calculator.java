376dc3bb9867ed2421ae2b49f72d30ecee78c35c
andrey.demchuk <andreichdem@gmail.com>
2016-12-15 12:21:05 +0200

    Calculator something work :)

--- src/module7/calculator/Calculator.java
+++ src/module7/calculator/Calculator.java
@@ -12,16 +12,31 @@ public class Calculator {
     public Calculator() {
         System.out.println("Enter your calculation, example: 3 + 3");
 
-        a = scanner.nextInt();
-        char[] chars = {'+', '-', '/', '*', '^'};
-        do {
-           aChar = scanner.next();
+        char[] chars = {'-', '+', '*', '/', '^'};
+        String str = scanner.nextLine();
+        String[] strings;
+        if (str.contains(" ")) {
+            strings = str.split(" ");
+            a = Integer.parseInt(strings[0]);
+            b = Integer.parseInt(strings[2]);
+            aChar = strings[1].charAt(0);
+        } else {
+            for (int i = 0; i < str.length(); i++) {
+                for (int j = 0; j < chars.length; j++) {
+                    if (str.charAt(i) == chars[j]) {
+                        a = Integer.parseInt(str.substring(0, i));
+                        aChar = str.charAt(i);
+                        b = Integer.parseInt(str.substring(i + 1));
+                    }
+                }
+            }
         }
-        b = scanner.nextInt();
+
         System.out.println("Your calculation is " + a + " " + aChar + " " + b);
     }
 
-    public int calculation(int a, char aChar, int b) {
+
+    public int calculation() {
 
         if (aChar == '+') {
             return sum(a, b);
@@ -33,22 +48,28 @@ public class Calculator {
             return division(a, b);
         } else if (aChar == '^') {
             return power(a, b);
-        } else
+        } else {
+            System.out.println("Something wrong");
             return 0;
-
+        }
     }
 
     private int power(int a, int b) {
         int result = a;
+        int counter = b;
         do {
             result = result * a;
-            b--;
-        } while (b != 1);
+            counter--;
+        } while (counter != 1);
         System.out.println(a + " to power " + b + "= " + result);
         return result;
     }
 
     private int division(int a, int b) {
+        if (b == 0) {
+            System.out.println("can't division by zero");
+            return 0;
+        }
         if (a % b == 0) {
             System.out.println("division= " + (a / b));
             return a / b;
@@ -69,7 +90,7 @@ public class Calculator {
     }
 
     private int difference(int a, int b) {
-        System.out.println("difference= " + (a - b);
+        System.out.println("difference= " + (a - b));
         return a - b;
     }
 
--- src/module7/calculator/TestCalculation.java
+++ src/module7/calculator/TestCalculation.java
@@ -3,6 +3,9 @@ package module7.calculator;
 public class TestCalculation {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
+
         calculator.calculation();
+
+
     }
 }
