b0fa6b2e010578b5a17252b97d4f1ab459ffc72d
andrey.demchuk <andreichdem@gmail.com>
2017-01-13 20:09:52 +0200

    practice module10

---
+++ src/module10/practice/Task.java
@@ -0,0 +1,32 @@
+package module10.practice;
+
+public class Task {
+    public static void main(String[] args) {
+        Integer a =20;
+        Number b =10;
+        Number c = 0;
+        try {
+            System.out.println(a/(Integer) b);
+        }catch (Exception e){
+            System.out.println(e);
+        }finally {
+            System.out.println(a/b.intValue());
+        }
+
+        try {
+            System.out.println(a/c.intValue());
+        } catch (ArithmeticException e) {
+            System.out.println("catch " + e);
+        }
+
+        try {
+            int[] arr = new int[2];
+            System.out.println(arr[arr.length]);
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("catch " + e);
+        }
+
+        
+
+    }
+}
