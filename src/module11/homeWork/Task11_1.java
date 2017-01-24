bf7e38e632093a655dc3cc823dacdb138b4698cd
andrey.demchuk <andreichdem@gmail.com>
2017-01-24 16:11:10 +0200

    module 11 completed

--- src/module11/homeWork/Task11_1.java
+++ src/module11/homeWork/Task11_1.java
@@ -1,31 +1,61 @@
 package module11.homeWork;
 
 import java.io.*;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Task11_1 {
     public static void main(String[] args) {
-        System.out.println(fileToString());
-        System.out.println("=======================================");
+        System.out.println("===================Original=======================");
+        System.out.println(fileToString(addStringInFile()));
+        System.out.println("==============String version replace==============");
         Map<String, String> stringMap = new HashMap<>();
         stringMap.put("one", "1");
         stringMap.put("two", "2");
         stringMap.put("three", "3");
-        System.out.println((replacer(stringMap)));
+        String replacer = replacer(stringMap);
+        System.out.println(replacer);
+        System.out.println("================File after replace================");
+        String fileToString = fileToString(fileWriter(replacer));
+        System.out.println(fileToString);
+        System.out.println("============File after append something============");
+        File file = fileAppendString(fileWriter(replacer), "Add new string");
+        System.out.println(fileToString(file));
+        System.out.println("==============Searching words in file==============");
+        checkWord(file, "Andrey");
+        checkWord(file, "not found");
+    }
 
+    private static File fileAppendString(File file, String s) {
+        try (FileWriter fileWriter = new FileWriter(file, true);
+        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter)) {
+            bufferedWriter.write("\n" +s);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return file;
     }
 
     public static String replacer(Map<String, String> map) {
-        String newString = fileToString();
+        String newString = fileToString(addStringInFile());
         for (Map.Entry<String, String> entry : map.entrySet()) {
             newString = newString.replaceAll(entry.getKey(), entry.getValue());
         }
         return newString;
     }
 
-    public static String fileToString() {
-        File file = addStringInFile();
+    public static File fileWriter(String str) {
+        File file = createNewFile();
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file))) {
+            bufferedWriter.write(str);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return file;
+    }
+
+    public static String fileToString(File file) {
         String fileToString = "";
         try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(
                 new FileInputStream(file)))) {
@@ -41,7 +71,7 @@ public class Task11_1 {
     }
 
     public static File createNewFile() {
-        File file = new File("D:\\JAVA\\TestIO\\TestInput.txt");
+        File file = new File("TestInput.txt");
         try {
             file.createNewFile();
         } catch (IOException e) {
@@ -62,5 +92,14 @@ public class Task11_1 {
         return file;
     }
 
-
+    public static int checkWord(File file, String word) {
+        int counter = 0;
+        String[] strFile = fileToString(file).replaceAll("\n", " ").split(" ");
+        for (String s : strFile) {
+            if (s.equals(word))
+                counter++;
+        }
+        System.out.println(file.getName() + " contains: " + counter + " \"" + word + "\" words");
+        return 0;
+    }
 }
