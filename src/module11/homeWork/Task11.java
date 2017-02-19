b93ab6ce1a7330abed68a77f57b6bf4f14671248
andrey.demchuk <andreichdem@gmail.com>
2017-02-20 01:38:36 +0200

    module 11 fixed

--- src/module11/homeWork/Task11.java
+++ src/module11/homeWork/Task11.java
@@ -16,10 +16,10 @@ public class Task11 {
         String replacer = replacer(stringMap);
         System.out.println(replacer);
         System.out.println("================File after replace================");
-        String fileToString = fileToString(fileWriter(replacer));
+        String fileToString = fileToString(fileWriter(replacer, false));
         System.out.println(fileToString);
         System.out.println("============File after append something============");
-        File file = fileAppendString(fileWriter(replacer), "Add new string");
+        File file = fileAppendString(fileWriter(replacer, false), "Add new string");
         System.out.println(fileToString(file));
         System.out.println("==============Searching words in file==============");
         checkWord(file, "Andrey");
@@ -37,7 +37,7 @@ public class Task11 {
         for (Map.Entry<String, String> entry : map.entrySet()) {
             replacedString = replacedString.replaceAll(entry.getKey(), entry.getValue());
         }
-        return fileWriter(replacedString);
+        return fileWriter(replacedString, true);
     }
 
     private static File fileAppendString(File file, String s) {
@@ -58,9 +58,12 @@ public class Task11 {
         return newString;
     }
 
-    public static File fileWriter(String str) {
+    public static File fileWriter(String str, boolean task) {
         File file = createNewFile();
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file))) {
+        try (   FileWriter fileWriter = new FileWriter(file, task);
+                BufferedWriter bufferedWriter = new BufferedWriter(fileWriter)) {
+            if (task)
+                bufferedWriter.write("\n");
             bufferedWriter.write(str);
         } catch (IOException e) {
             e.printStackTrace();
