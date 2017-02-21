3d7744aa16eb7f57e06d979b23e38c21ed0353b3
andrey.demchuk <andreichdem@gmail.com>
2017-02-21 19:59:45 +0200

    2 semestr\module1

---
+++ src/java2Module/hw2/4.sql
@@ -0,0 +1,6 @@
+#ALTER TABLE projects ADD cost int;
+
+UPDATE projects SET cost = 8500 WHERE id_project = 1;
+UPDATE projects SET cost = 3500 WHERE id_project = 2;
+UPDATE projects SET cost = 5500 WHERE id_project = 3;
+UPDATE projects SET cost = 9000 WHERE id_project = 4;
